e0ddd4e88f40da4dfb71b53ef47222787aa21855
GinMUT <6811830012@mut.ac.th>
2025-12-30 16:26:03 +0700

    อัพเดท code LAB 07

---
+++ LAB 07/Address และ Pointer/Address และ Pointer.cpp
@@ -0,0 +1,16 @@
+﻿// TODO: แสดงค่า score, address และ pointer
+#include <iostream>
+using namespace std;
+int main() {
+	int score = 50;
+	int* ptr = &score;
+	// TODO
+	cout << score << endl;
+	cout << *ptr << endl;
+
+	cout << "Score: " << score << endl;
+	cout << "Address of score: " << &score << endl;
+	cout << "Pointer ptr points to address: " << ptr << endl;
+
+	return 0;
+}
